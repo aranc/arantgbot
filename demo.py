4aa39e3eaf5d6d1d73c5193a8f634cda54c62a89
Aran Carmon <aran.carmon@gmail.com>
2017-02-10 09:00:42 +0200

    more

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from bot import go_online, register_command
+import pexpect
 
 def repeat(cmd, parameters):
     return "the parameters are: <"+parameters+">"
@@ -10,10 +11,13 @@ def smile(cmd, parameters):
 def silent(cmd, parameters):
     print "ssshhh"
 
-#todo: ddate/ls
+def shell_word(cmd, parameters):
+    return pexpect.run(cmd)
 
 register_command("repeat", repeat)
 register_command("smile", smile)
 register_command("silent", silent)
+register_command("ls", shell_word)
+register_command("ddate", shell_word)
 
 go_online()
