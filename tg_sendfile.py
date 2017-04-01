315fee588094dc447550c998886e05b57df184b2
Aran Carmon <aran.carmon@gmail.com>
2017-04-01 13:59:10 +0000

    tg_sendfile.py

---
+++ tg_sendfile.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from twx.botapi import TelegramBot, ReplyKeyboardMarkup, InputFileInfo, InputFile
+import os
+import sys
+import time
+
+with open(os.path.expanduser("~/arantgbot.api_token"), 'r') as _file:
+    api_token = _file.readline().strip()
+
+with open(os.path.expanduser("~/arantgbot.my_user_id"), 'r') as _file:
+    my_user_id = int(_file.readline().strip())
+
+bot = TelegramBot(api_token)
+fp = open(os.path.expanduser(sys.argv[1]), 'rb')
+file_info = InputFileInfo(sys.argv[1], fp, "")
+inputfile=InputFile('document', file_info)
+bot.send_document(chat_id=my_user_id, document=inputfile)
