dea1efffae5fe4b6fa7483c98befe365291fa6e6
Aran Carmon <aran.carmon@gmail.com>
2017-04-08 12:22:41 +0300

    demo: run process in background

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
-from bot import go_online, register_command
+import sys
+import time
+import os
 import pexpect
+from bot import go_online, register_command
 
 def repeat(cmd, parameters):
     return "the parameters are: <"+parameters+">"
@@ -14,10 +17,21 @@ def silent(cmd, parameters):
 def shell_word(cmd, parameters):
     return pexpect.run(cmd)
 
+def spam(cmd, parameters):
+    newpid = os.fork()
+    if newpid != 0: return
+    print "spawning spam"
+    cmd = "~/arantgbot/spam_demo.py " + str(parameters)
+    p = pexpect.spawn('bash -c "'+cmd+'"', logfile=sys.stdout)
+    p.interact()
+    print "spam spinning stopped"
+    sys.exit()
+
 register_command("repeat", repeat)
 register_command("smile", smile)
 register_command("silent", silent)
 register_command("ls", shell_word)
 register_command("ddate", shell_word)
+register_command("spam", spam)
 
 go_online()
---
+++ spam_demo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from twx.botapi import TelegramBot, ReplyKeyboardMarkup
+import sys
+import os
+import time
+
+print "spam spawned"
+
+with open(os.path.expanduser("~/arantgbot.api_token"), 'r') as _file:
+    api_token = _file.readline().strip()
+
+with open(os.path.expanduser("~/arantgbot.my_user_id"), 'r') as _file:
+    my_user_id = int(_file.readline().strip())
+
+bot = TelegramBot(api_token)
+for i in range(100):
+    time.sleep(1)
+    msg = str(i) + " " + " ".join(sys.argv[1:])
+    print "spammm:", msg
+    bot.send_message(my_user_id, msg).wait()
