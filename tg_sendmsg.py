de1fbdbd43ded283307a148621b01f7ac3c3c3f1
Aran Carmon <aran.carmon@gmail.com>
2018-06-30 08:26:08 +0000

    tg_sendmsg: Parse newline characters

--- tg_sendmsg.py
+++ tg_sendmsg.py
@@ -11,4 +11,6 @@ with open(os.path.expanduser("~/arantgbot.my_user_id"), 'r') as _file:
     my_user_id = int(_file.readline().strip())
 
 bot = TelegramBot(api_token)
-bot.send_message(my_user_id, " ".join(sys.argv[1:])).wait()
+msg = " ".join(sys.argv[1:])
+msg = msg.replace('\\n', '\n')
+bot.send_message(my_user_id, msg).wait()
